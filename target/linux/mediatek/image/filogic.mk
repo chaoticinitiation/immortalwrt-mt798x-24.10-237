8c71c4143d3d11d79dfaa2dfd94ced9f6b9b7465
chaoticinitiation <79523032+chaoticinitiation@users.noreply.github.com>
2026-04-08 20:37:19 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -615,7 +615,7 @@ define Device/philips_hy3000
   DEVICE_DTS := mt7981b-philips-hy3000
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-usb3 f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
-    luci-app-samba4
+    luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
   SUPPORTED_DEVICES += philips,hy3000
   KERNEL := kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
   KERNEL_INITRAMFS := kernel-bin | lzma | \
